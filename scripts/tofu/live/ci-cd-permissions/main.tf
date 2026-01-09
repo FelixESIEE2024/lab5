1f0038aaa3dd101a0e3e044009f287ee7c7bdb63
TonNomGitHub <ton.email@exemple.com>
2026-01-09 13:52:45 +0100

    Point OIDC modules to my repo and use existing state

--- scripts/tofu/live/ci-cd-permissions/main.tf
+++ scripts/tofu/live/ci-cd-permissions/main.tf
@@ -2,29 +2,36 @@ provider "aws" {
   region = "us-east-2"
 }
 
+
+
+
+#ETABLIT LA RELATION DE CONFIANCE AWSH GITHUB 
 module "oidc_provider" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/github-aws-oidc"
+  # OIDC provider hosted on GitHub (your repo path)
+  source = "github.com/FelixESIEE2024/lab5//scripts/tofu/modules/github-aws-oidc"
 
-  provider_url = "https://token.actions.githubusercontent.com" 
-
+  provider_url = "https://token.actions.githubusercontent.com"
 }
 
+
+
+#GESTION DES DROITS
 module "iam_roles" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/gh-actions-iam-roles"
+  # IAM roles for GitHub Actions (your repo path)
+  source = "github.com/FelixESIEE2024/lab5//scripts/tofu/modules/gh-actions-iam-roles"
 
-  name              = "lambda-sample"                           
-  oidc_provider_arn = module.oidc_provider.oidc_provider_arn    
+  name              = "lambda-sample"
+  oidc_provider_arn = module.oidc_provider.oidc_provider_arn
 
-  enable_iam_role_for_testing = true                            
+  enable_iam_role_for_testing = true
+  enable_iam_role_for_plan    = true
+  enable_iam_role_for_apply   = true
 
-  # TODO: fill in your own repo name here!
-  github_repo      = "brikis98/fundamentals-of-devops-examples" 
-  lambda_base_name = "lambda-sample"                            
+  # Votre dépôt GitHub
+  github_repo      = "FelixESIEE2024/lab5"
+  lambda_base_name = "lambda-sample"
 
-  enable_iam_role_for_plan  = true                                
-  enable_iam_role_for_apply = true                                
-
-  # TODO: fill in your own bucket and table name here!
-  tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
-  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
+  # Backend state déjà créé
+  tofu_state_bucket         = "felixesiee2024-devops-tofu-state"
+  tofu_state_dynamodb_table = "felixesiee2024-devops-tofu-state"
 }
--- scripts/tofu/live/tofu-state/main.tf
+++ scripts/tofu/live/tofu-state/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 module "state" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/state-bucket"
+  source = "github.com/FelixESIEE2024/lab5//scripts/tofu/modules/state-bucket"
 
-  # TODO: fill in your own bucket name!
-  name = "fundamentals-of-devops-tofu-state"
+  # Bucket unique déjà créé
+  name = "felixesiee2024-devops-tofu-state"
 }
