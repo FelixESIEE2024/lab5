12a52a28b534520d1c23ba6ea630121a33076198
TonNomGitHub <ton.email@exemple.com>
2026-01-10 10:10:29 +0100

    Add infrastructure tests workflow

--- scripts/sample-app/app.js
+++ scripts/sample-app/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('HAHAHAHAHHAHAHA');
+  res.send('HAHAHAHHAHHA');
 });
 
 app.get('/name/:name', (req, res) => {
