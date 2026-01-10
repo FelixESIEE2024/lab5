9a6243f006d83648d0049c4c2d90bde0586b2864
TonNomGitHub <ton.email@exemple.com>
2026-01-10 20:48:01 +0100

    ajout robots

--- scripts/tofu/live/ci-cd-permissions/main.tf
+++ scripts/tofu/live/ci-cd-permissions/main.tf
@@ -3,28 +3,33 @@ provider "aws" {
 }
 
 module "oidc_provider" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/github-aws-oidc"
-
+  #source = "github.com/brikis98/devops-book//ch5/tofu/modules/github-aws-oidc"
+  source = "github.com/FelixESIEE2024/lab5//scripts/tofu/modules/github-aws-oidc"
   provider_url = "https://token.actions.githubusercontent.com" 
 
 }
 
 module "iam_roles" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/gh-actions-iam-roles"
+  #source = "github.com/brikis98/devops-book//ch5/tofu/modules/gh-actions-iam-roles"
+
+
+
+  source = "github.com/FelixESIEE2024/lab5//scripts/tofu/modules/gh-actions-iam-roles"
 
   name              = "lambda-sample"                           
   oidc_provider_arn = module.oidc_provider.oidc_provider_arn    
 
   enable_iam_role_for_testing = true                            
+  enable_iam_role_for_plan  = true                                
+  enable_iam_role_for_apply = true
 
-  # TODO: fill in your own repo name here!
-  github_repo      = "brikis98/fundamentals-of-devops-examples" 
+  #github_repo      = "brikis98/fundamentals-of-devops-examples" 
+  github_repo      = "FelixESIEE2024/lab5" 
+
   lambda_base_name = "lambda-sample"                            
 
-  enable_iam_role_for_plan  = true                                
-  enable_iam_role_for_apply = true                                
+                                
 
-  # TODO: fill in your own bucket and table name here!
-  tofu_state_bucket         = "fundamentals-of-devops-tofu-state" 
-  tofu_state_dynamodb_table = "fundamentals-of-devops-tofu-state" 
+  tofu_state_bucket         = "devops-td5-tofu-state-felix-test-depuis-git"
+  tofu_state_dynamodb_table = "devops-td5-tofu-state-felix-test-depuis-git" 
 }
--- scripts/tofu/live/lambda-sample/backend.tf
+++ scripts/tofu/live/lambda-sample/backend.tf
@@ -1,9 +1,11 @@
+
+
 terraform {
-  backend "s3" {
-    bucket         = "devops-td5-tofu-state-felix-test-depuis-git"
-    key            = "td5/scripts/tofu/live/lambda-sample"
-    region         = "us-east-2"
-    encrypt        = true
-    dynamodb_table = "devops-td5-tofu-state-felix-test-depuis-git"
-  }
-}
+ 	backend "s3" {
+ 		bucket         = "devops-td5-tofu-state-felix-test-depuis-git"
+ 		key            = "lambda-sample/terraform.tfstate"
+ 		region         = "us-east-2"
+ 		encrypt        = true
+ 		dynamodb_table = "devops-td5-tofu-state-felix-test-depuis-git"
+ 	}
+ }
