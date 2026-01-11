c2148faf4dd506e0bc79314dfbd0536173947494
TonNomGitHub <ton.email@exemple.com>
2026-01-11 14:55:21 +0100

    modif test mineur

--- scripts/sample-app/app.js
+++ scripts/sample-app/app.js
@@ -4,8 +4,10 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  //res.send('Hello, World!');
+  res.send('Hello, FELIX!');
 });
+
 
 app.get('/name/:name', (req, res) => {
   res.render('hello', {name: req.params.name});
